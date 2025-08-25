5e40dc190ad47c1d35c3e546791b599f98da9d09
Schamel <Justin.Schamel@parsons.us>
2025-08-25 08:36:15 -0400

    Made a basic pong game as part of the 100 days of code challange.

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
-# Import required modules
 from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from time import sleep
 
-# Instantiate screen object
 screen = Screen()
-
-# Define the attributes of the screen object
-screen.bgcolor("black")
 screen.setup(width=800, height=600)
-screen.title("Pong")
+screen.bgcolor("black")
+screen.title("The Great Game of Pong")
 screen.tracer(0)
 
-# Instantiate turtle object
-paddle = Turtle()
-
-# Define attributes of paddle object
-paddle.penup()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.teleport(350, 0)
-
-# Define methods of paddle object
-
-def go_up():
-    paddle.sety(paddle.ycor() + 20)
-
-def go_down():
-    paddle.sety(paddle.ycor() - 20)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+score_board = Turtle()
+score_board.hideturtle()
+score_board.color("white")
+score_board.penup()
+score_board.goto(0, 250)
+score = 0
 
 screen.listen()
-screen.onkeypress(go_up, "Up")
-screen.onkeypress(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
-# Refresh screen while game engine is running
-game_on = True
-while game_on:
+game_is_on = True
+while game_is_on:
+    sleep(0.1)
     screen.update()
+    score_board.clear()
+    score_board.write(f"Score: {score}", align="center", font=("ariel", 24, "bold"))
+    ball.move()
 
-# Exit screen on event
+    # Detect collisions with ceiling
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collisions with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() < 330 or ball.distance(l_paddle) < 50 and ball.xcor() < -330:
+        ball.bounce_x()
+        score += 1
+
+    # Detect out of bounds
+    if ball.xcor() > 380 or ball.xcor() < -380:
+        pen = Turtle()
+        pen.penup()
+        pen.goto(0, 0)
+        pen.color("red")
+        pen.write("GAME OVER MAN!!!", align="center", font=("ariel", 48, "bold"))
+        game_is_on = False
+
+
 screen.exitonclick()
